149e427a3a77e61c3ce45e656f633fb3a5735de7
vngomez <vn.gomez@uniandes.edu.co>
2018-10-05 20:44:06 -0500

    intento registro/login

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,6 +4,9 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from './Navbar.jsx';
 import Inicio from './Inicio.jsx';
 import Puntos from './Puntos.jsx';
+import Registro from './Registro.jsx';
+import Login from './Login.jsx';
+
 
 class App extends Component {
   render() {
@@ -15,6 +18,8 @@ class App extends Component {
             <Route exact path="/" component={Inicio} />
             <Route exact path="/beneficios" component={CatalogoBeneficios} />
             <Route exact path="/puntos" component={Puntos} />
+            <Route exact path="/registro" component={Registro} />
+            <Route exact path="/login" component={Login} />
             <Redirect from="*" to="/" />
           </Switch>
         </div>
--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ import {
   Meteor
 } from 'meteor/meteor';
 import '../imports/api/beneficios.js';
+import '../imports/api/usuarios.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
